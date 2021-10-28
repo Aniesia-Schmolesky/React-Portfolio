9bc4efe86a3695c0ddefb0cb1edbbf83cb91b76c
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-10-28 15:49:45 -0700

    added about me content

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import React from "react";
 import AboutMe from "./components/AboutMe";
 import ContactMe from "./components/ContactMe";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Aniesia Schmolesky's Portfolio
+      </div>
+    );
+  }
+}
+
 export default function App() {
   return (
     <main>
-      <Navbar />
+      <Header />
+      <Project />
+      <Footer />
       <AboutMe />
       <Projects />
       <ContactMe />
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,1 +1,10 @@
-export default function Projects() {}
+export default function Projects() {
+    return(
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+                < div class="flex flex-wrap -m-4">
+                </div>
+            </div> 
+        </section>
+    )
+}

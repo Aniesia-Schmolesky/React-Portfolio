5c6d698770775e4b4529202820f62be3b39724bf
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-10-28 14:42:41 -0700

    linked components to App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import React from "react";
+import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
     <main>
       <Navbar />
-      <About />
+      <AboutMe />
       <Projects />
-      <Contact />
+      <ContactMe />
       <Resume />
     </main>
   );

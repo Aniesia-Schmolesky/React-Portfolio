d777a9b0149a683bb36cf677f740b5b39db3bbf0
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-10-28 19:33:31 -0700

    ask BCS helped link images

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,3 +1,10 @@
+import fitnessTracker from './images/fitness-tracker.png'
+import geekEmporium from './images/geek-emporium.PNG'
+import inspirationStation from './images/inspiration-station.png'
+import jsQuiz from './images/js-quiz.png'
+import passwordGenerator from './images/password-generator.png'
+import teamProfilePic from './images/team-profile-generator.png'
+
 export default function Projects() {
     return(
         <section class="text-gray-600 body-font">
@@ -6,7 +13,7 @@ export default function Projects() {
 
                     <div class="p-4 md:w-1/3">
                     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"/>
+                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={inspirationStation} alt="blog"/>
                     <div class="p-6">
                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Inspiration Station</h1>
                     <div class="flex items-center flex-wrap ">
@@ -31,7 +38,7 @@ export default function Projects() {
 
                     <div class="p-4 md:w-1/3">
                     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"/>
+                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={fitnessTracker} alt="blog"/>
                     <div class="p-6">
                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">NoSQL Fitness Tracker</h1>
                     <div class="flex items-center flex-wrap ">
@@ -56,7 +63,7 @@ export default function Projects() {
 
                     <div class="p-4 md:w-1/3">
                     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"/>
+                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={geekEmporium} alt="blog"/>
                     <div class="p-6">
                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Geek Emporium</h1>
                     <div class="flex items-center flex-wrap ">
@@ -81,7 +88,7 @@ export default function Projects() {
 
                     <div class="p-4 md:w-1/3">
                     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"/>
+                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={jsQuiz} alt="blog"/>
                     <div class="p-6">
                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">JavaScript Quiz</h1>
                     <div class="flex items-center flex-wrap ">
@@ -106,7 +113,7 @@ export default function Projects() {
 
                     <div class="p-4 md:w-1/3">
                     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"/>
+                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={passwordGenerator} alt="blog"/>
                     <div class="p-6">
                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Password Generator</h1>
                     <div class="flex items-center flex-wrap ">
@@ -131,7 +138,7 @@ export default function Projects() {
 
                     <div class="p-4 md:w-1/3">
                     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={"./images/team-profile-generator.png"} alt="blog"/>
+                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src={teamProfilePic} alt="blog"/>
                     <div class="p-6">
                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Team Profile Generator</h1>
                     <div class="flex items-center flex-wrap ">

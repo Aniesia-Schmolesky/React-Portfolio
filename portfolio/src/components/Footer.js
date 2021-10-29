3a03d33b71ff685f57c68e28c9000686bf1430ef
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-10-28 18:19:58 -0700

    added projects to project page

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
       <span class="ml-3 text-xl">Aniesia Schmolesky</span>
     </a>
     <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">Created Oct. 28, 2021 —
-      <a href="https://twitter.com/knyttneve" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@knyttneve</a>
+      <a class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">aniesiaschmoleskya@gmail.com</a>
     </p>
     <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
       <a class="text-gray-500">
